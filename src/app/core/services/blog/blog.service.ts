06de0211d6905a1b15338728594fd5f8975735ce
unknown <hristozafirov96@gmail.com>
2018-06-03 20:47:03 +0300

    Project v1.4

--- src/app/core/services/blog/blog.service.ts
+++ src/app/core/services/blog/blog.service.ts
@@ -15,24 +15,24 @@ export class BlogService {
     const heroes: BlogViewModel[] = [
       {
         Id: 'bcfacd2f-4143-4717-8720-b00cd6e8e859',
-        Title: 'Eat wisely!',
-        Description: 'Somethimes it\'s good idea to go to bed hungry',
-        Author: 'Mr. Nice',
-        ImgSrc: './assets/images/sleep-hungry.jpg'
+        Title: 'There is a protein named after Pikachu',
+        Description: 'Pikachurin is a protein found in humans that is used for transmitting electrical impulses from the human eyes to the brain.',
+        Author: 'Ch. Zafirov',
+        ImgSrc: './assets/images/Pikachu-Protein.jpg'
       },
       {
         Id: 'e4445009-3ca3-40af-aa97-abafc8727501',
-        Title: 'Why Americans are so fat?',
-        Description: 'Eating burgers all the time is not the ultimate skinny problem solving...',
-        Author: 'Narco',
-        ImgSrc: './assets/images/americans.jpg'
+        Title: 'Pikachu has an obsession with Ketchup',
+        Description: 'Not only has it been shown in the animated series at various times, but there are entire threads in online message boards and whole memes dedicated to Pikachu and his love for ketchup.',
+        Author: 'Ch. Zafirov',
+        ImgSrc: './assets/images/Pikachu-Ketchup.jpg'
       },
       {
         Id: '6cd12f8e-9b8f-4c6a-9779-7b1de49d7a71',
-        Title: 'How to eat octopus',
-        Description: 'You should be aware of this 5 simple steps to eat octopus....',
-        Author: 'Chief Skull Master ',
-        ImgSrc: './assets/images/octopus.jpg'
+        Title: 'Female Pikachu Have a Notch in Their Tails',
+        Description: 'Since Pokemon Diamond and Pearl gender difference was released in the Pokemon series introducing different shapes on different species.',
+        Author: 'Ch. Zafirov',
+        ImgSrc: './assets/images/Female-Pikachu.jpg'
       },
     ];
     return heroes;
