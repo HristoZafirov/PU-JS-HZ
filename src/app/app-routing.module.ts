8106893775e2c391fc2e6f6a91d597bc8fc8931a
Hristo Zafirov <hristozafirov96@gmail.com>
2018-06-10 21:01:39 +0300

    Added gallery routing and filled gallery component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,9 @@ import { NotFoundComponent } from './partials/not-found/not-found.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutComponent } from './about/about.component';
 import { BlogComponent } from './blog/blog.component';
-import { ItalianFoodsComponent } from './recipes/italian-foods/italian-foods.component';
-import { SpanishFoodsComponent } from './recipes/spanish-foods/spanish-foods.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes =
 [
@@ -16,8 +15,7 @@ const routes: Routes =
 { path: 'home', component: LandingPageComponent },
 { path: 'about', component: AboutComponent },
 { path: 'blog', component: BlogComponent },
-{ path: 'italian', component: ItalianFoodsComponent },
-{ path: 'spanish', component: SpanishFoodsComponent },
+{ path: 'gallery', component: GalleryComponent},
 { path: 'login', component: LoginComponent },
 { path: 'register', component: RegisterComponent },
 { path: '**', component: NotFoundComponent }
